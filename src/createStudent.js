3316154c1492474df7b155f8b5986b221a19a00e
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-05-03 20:29:32 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome, frase) => {
+let student = {
+  name: nome,
+  feedback() { 
+  return 'Eita pessoa boa!';
+},
+};
+
+return student;
+};
 
 module.exports = createStudent;
